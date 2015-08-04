bbecd88bac87103d189209c1fde75e4d3eae304c
aznkid66 <aznkid66@gmail.com>
2015-08-04 08:10:08 -0400

    basic incomplete url stuff

--- app/src/main/java/com/example/patrick/recipeapp2/MainActivity.java
+++ app/src/main/java/com/example/patrick/recipeapp2/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.patrick.recipeapp2;
 
-import android.support.v7.app.ActionBarActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    final String API_KEY = "1ceebd1c1ae69bbeaa5b6a18aa987aab";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        test();
     }
 
 
@@ -35,5 +45,32 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void test() {
+        ArrayList<String> ingredients = new ArrayList<String>(
+                Arrays.asList("apple", "banana", "corn")
+        );
+
+        // will contain raw JSON response as a string
+        String searchJsonStr = null;
+        try {
+            final String SEARCH_BASE_URL = "http://food2fork.com/api/search?";
+            final String KEY_PARAM = "key";
+            final String QUERY_PARAM = "q";
+
+            Uri builtUri = Uri.parse(SEARCH_BASE_URL).buildUpon()
+                    .appendQueryParameter(KEY_PARAM, API_KEY)
+                    .appendQueryParameter(QUERY_PARAM, "")
+                    .build();
+
+            URL url = new URL(builtUri.toString());
+
+
+        } catch (IOException e) {
+
+        } finally {
+
+        }
     }
 }
