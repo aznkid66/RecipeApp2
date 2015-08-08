a0838a5a88867e54297c82df2deab63651541441
Angela Wang <angelawang17@gmail.com>
2015-08-08 10:43:19 +0800

    added loader

--- app/src/main/java/com/example/patrick/recipeapp2/SearchResultsFragment.java
+++ app/src/main/java/com/example/patrick/recipeapp2/SearchResultsFragment.java
@@ -78,7 +78,7 @@ public class SearchResultsFragment extends Fragment {
         mListView.setAdapter(mRecipesAdapter);
         // this is for the clicked open page
         // We'll call our MainActivity
-//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //
 //            @Override
 //            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
@@ -94,8 +94,89 @@ public class SearchResultsFragment extends Fragment {
 //                }
 //                mPosition = position;
 //            }
-//        });
+        });
+
+        // If there's instance state, mine it for useful information.
+        // The end-goal here is that the user never knows that turning their device sideways
+        // does crazy lifecycle related things.  It should feel like some stuff stretched out,
+        // or magically appeared to take advantage of room, but data or place in the app was never
+        // actually *lost*.
+        if (savedInstanceState != null && savedInstanceState.containsKey(SELECTED_KEY)) {
+            // The listview probably hasn't even been populated yet.  Actually perform the
+            // swapout in onLoadFinished.
+            mPosition = savedInstanceState.getInt(SELECTED_KEY);
+        }
+
+        mRecipesAdapter.setUseTodayLayout(mUseTodayLayout);
+
 
         return rootView;
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(RECIPE_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        // When tablets rotate, the currently selected list item needs to be saved.
+        // When no item is selected, mPosition will be set to Listview.INVALID_POSITION,
+        // so check for that before storing.
+        if (mPosition != ListView.INVALID_POSITION) {
+            outState.putInt(SELECTED_KEY, mPosition);
+        }
+        super.onSaveInstanceState(outState);
+    }
+    // sort by popularity of the recipe
+//    @Override
+//    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+//        // This is called when a new Loader needs to be created.  This
+//        // fragment only uses one loader, so we don't care about checking the id.
+//
+//        // To only show current and future dates, filter the query to return weather only for
+//        // dates after or including today.
+//
+//        // Sort order:  Ascending, by date.
+//        String sortOrder = WeatherContract.WeatherEntry.COLUMN_DATE + " ASC";
+//
+//        String locationSetting = Utility.getPreferredLocation(getActivity());
+//        Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
+//                locationSetting, System.currentTimeMillis());
+//
+//        return new CursorLoader(getActivity(),
+//                weatherForLocationUri,
+//                FORECAST_COLUMNS,
+//                null,
+//                null,
+//                sortOrder);
+//    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mRecipesAdapter.swapCursor(data);
+        if (mPosition != ListView.INVALID_POSITION) {
+            // If we don't need to restart the loader, and there's a desired position to restore
+            // to, do so now.
+            mListView.smoothScrollToPosition(mPosition);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mRecipesAdapter.swapCursor(null);
+    }
+
+    public void setUseTodayLayout(boolean useTodayLayout) {
+        mUseTodayLayout = useTodayLayout;
+        if (mRecipesAdapter != null) {
+            mRecipesAdapter.setUseTodayLayout(mUseTodayLayout);
+        }
+    }
+
+
+
+
 }
