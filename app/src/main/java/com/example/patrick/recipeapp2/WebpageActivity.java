33907932e359561ee61c51c67bc27522640b30a0
aznkid66 <aznkid66@gmail.com>
2015-08-11 17:30:40 -0700

    fixed redirect issue

--- app/src/main/java/com/example/patrick/recipeapp2/WebpageActivity.java
+++ app/src/main/java/com/example/patrick/recipeapp2/WebpageActivity.java
@@ -3,6 +3,7 @@ package com.example.patrick.recipeapp2;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebpageActivity extends ActionBarActivity{
     @Override
@@ -11,6 +12,12 @@ public class WebpageActivity extends ActionBarActivity{
         setContentView(R.layout.activity_webpage);
 
         WebView myWebView = (WebView) findViewById(R.id.activity_webpage);
+        myWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
         myWebView.loadUrl(getIntent().getStringExtra("URL"));
 
     }
